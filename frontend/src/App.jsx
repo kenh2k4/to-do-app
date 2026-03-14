63cbfaca98eb0141a239aceae4f071df2ddb4a82
kenh2k4 <kennyh_2004@hotmail.com>
2026-03-14 17:44:29 -0400

    add loading spinner for render sleep

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,27 @@ import { getTasks, deleteTask } from './api/tasks';
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [modalOpen, setModalOpen] = useState(false);
   const [editingTask, setEditingTask] = useState(null);
 
   const fetchTasks = async () => {
-    const data = await getTasks();
-    setTasks(data);
-  };
+    try {
+      setLoading(true);
 
+      const data = await getTasks();
+
+      setTasks(data);
+      setLoading(false);
+
+    } catch (err) {
+      console.log("Backend not ready, retrying...");
+
+      // retry after 2 seconds
+      setTimeout(loadTasks, 2000);
+    }
+  };
+  
   useEffect(() => {
     fetchTasks();
   }, []);
@@ -31,6 +44,15 @@ export default function App() {
     await deleteTask(id);
     fetchTasks();
   };
+
+  if (loading) {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h2>Waking up server...</h2>
+        <div className="spinner"></div>
+      </div>
+    );
+  }
 
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
